d8806aa5beb640788cb784155b39507b635c68e5
zoemeriet <zoe@binch-solutions.com>
2022-05-08 22:24:01 +0200

    add about

--- components/iframe/IframeGroup.vue
+++ components/iframe/IframeGroup.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="iframegroup--component">
-    <h2 class="iframegroup--title">{{ props.title }}</h2>
-    <p class="iframegroup--subtitle">{{ props.subtitle }}</p>
+    <div class="iframegroup--content">
+      <h2 class="iframegroup--title">{{ props.title }}</h2>
+      <p class="iframegroup--subtitle">{{ props.subtitle }}</p>
+    </div>
     <div class="iframegroup--iframes">
       <Iframe v-for="iframe in props.iframes" :id="iframe" />
     </div>
@@ -25,24 +27,54 @@ const props = defineProps({
 </script>
 
 <style>
+.iframegroup--component {
+  margin-bottom: 40px;
+}
+
 .iframegroup--iframes {
   display: grid;
   grid-template-columns: 1fr;
   gap: 30px;
 }
+
 .iframegroup--title {
-  font-size: 40px;
+  font-size: 30px;
   font-weight: bold;
   margin: 0;
+  margin-bottom: 20px;
 }
+
 .iframegroup--subtitle {
   color: #FAFAFA;
   font-weight: lighter;
   margin: 0;
+  margin-bottom: 20px;
 }
-@media (min-width: 900px) {
+
+@media (min-width: 768px) {
   .iframegroup--iframes {
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (min-width: 1024px) {
+  .iframegroup--component {
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 80px;
+  }
+
+  .iframegroup--content {
+    width: 350px;
+    margin-right: 20px;
+  }
+
+  .iframegroup--title {
+    font-size: 50px;
+  }
+
+  .iframegroup--iframes {
+    flex: 1;
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <div>Coucou</div>
+    <div class="about">
+      <h1 class="about--title">Eliot Meriet</h1>
+      <img class="about--image" src="~/assets/image_profil.jpg" />
+      <div class="about--text">
+        <p>Je m'appelle Eliot Meriet, étant passionné de vidéo et de montage, je suis actuellement en 2ème année d'école
+          de cinéma / audiovisuel.</p>
+        <p>Je n'ai qu'un seul désir, en faire mon métier.</p>
+      </div>
+    </div>
     <div class="projects">
-      <IframeGroup v-for="iframegroup in data.iframegroup" :title="iframegroup.title" :subtitle="iframegroup.subtitle" :iframes="iframegroup.iframes" />
+      <h2 class="projects--title">Mes projets</h2>
+      <IframeGroup v-for="iframegroup in data.iframegroup" :title="iframegroup.title" :subtitle="iframegroup.subtitle"
+        :iframes="iframegroup.iframes" />
     </div>
   </div>
 </template>
@@ -17,17 +27,17 @@ const data = {
     },
     {
       title: "Vlog",
-      subtitle: "Réflexion et coréalisation de chaque vidéo. Je fais tout les montages de chacune de vidéos de la chaine",
+      subtitle: "Réflexion et coréalisation de chaque vidéo. Je fais tous les montages de chacune des vidéos de la chaine",
       iframes: ["oA6UeIhlZAQ", "j9fAzkGdfrI", "bee1c-Nl0aw", "rSQe1r_hwPc"]
     },
     {
       title: "Court métrage",
-      subtitle: "participation a toute la prépa, au tournage. Et réalisation du montage",
+      subtitle: "Participation à toute la préparation, au tournage. Et réalisation du montage",
       iframes: ["c03tiO8vnHY"]
     },
     {
       title: "Documentaire",
-      subtitle: "participation a toute la prépa, au tournage. Et réalisation du montage",
+      subtitle: "Participation à toute la préparation, au tournage. Et réalisation du montage",
       iframes: ["rAf54ryHW5I"]
     }
   ]
@@ -35,9 +45,80 @@ const data = {
 </script>
 
 <style>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+
+.about--title {
+  font-size: 40px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 30px;
+}
+
+.about--image {
+  max-width: 100%;
+  width: 400px;
+}
+
+.about--text {
+  font-size: 18px;
+  max-width: 500px;
+}
+
 .projects {
   padding: 20px;
   color: #FFFFFF;
   background-color: #1F2933;
+}
+
+.projects--title {
+  font-size: 40px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 30px;
+  color: #384149;
+}
+
+@media (min-width: 1024px) {
+  .about {
+    flex-direction: row;
+    padding: 40px;
+    padding-top: 110px;
+  }
+
+  .about--title {
+    position: absolute;
+    top: 0;
+    left: -10px;
+    font-size: 130px;
+    z-index: -1;
+    color: #E6E7E8;
+    opacity: 0.4;
+  }
+
+  .about--image {
+    margin-right: 30px;
+  }
+
+  .projects {
+    position: relative;
+    padding: 40px;
+    padding-top: 110px;
+  }
+
+  .projects--title {
+    position: absolute;
+    top: 0;
+    right: -10px;
+    font-size: 130px;
+  }
 }
 </style>
