b728ad2fdc01894738ca7584b6a7c777c63b13c2
zoemeriet <zoe.meriet112@gmail.com>
2023-12-18 21:28:43 +0100

    new version

---
+++ pages/[id].vue
@@ -0,0 +1,49 @@
+<template>
+  <div v-if="dataProject" class="project--page">
+    {{ dataProject.title }}
+    <img :src="dataProject.head" :alt="dataProject.title">
+    <video controls class="project--video">
+      <source :src="dataProject.video" type="video/mp4" />
+    </video>
+    <div class="project--stills">
+      <img v-for="still in dataProject.stills" :src="still" :alt="dataProject.title" class="project--img">
+    </div>
+  </div>
+  <div v-else>
+    No data
+  </div>
+</template>
+
+<script lang="ts" setup>
+import data from "../data.json";
+const route = useRoute();
+const dataProject = data.projects.find(project => project.slug === route.params.id)
+</script>
+
+<style>
+.project--page {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  max-width: 1200px;
+  margin: auto;
+  padding: 8px;
+}
+.project--video {
+  width: 100%;
+  height: auto;
+}
+.project--stills {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 8px;
+}
+.project--img {
+  width: 100%;
+  height: auto;
+}
+
+@media (min-width: 1024px) {
+
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,124 +1,51 @@
 <template>
   <div>
-    <div class="about">
-      <h1 class="about--title">Eliot Meriet</h1>
-      <img class="about--image" src="~/assets/image_profil.jpg" />
-      <div class="about--text">
-        <p>Je m'appelle Eliot Meriet, étant passionné de vidéo et de montage, je suis actuellement en 2ème année d'école
-          de cinéma / audiovisuel.</p>
-        <p>Je n'ai qu'un seul désir, en faire mon métier.</p>
-      </div>
+    <div class="projects--header">
+      <h1 class="projects--title">Eliot Meriet</h1>
+      <!-- <video autoplay muted loop class="projects--video">
+        <source :src="data.video" type="video/mp4">
+      </video> -->
     </div>
-    <div class="projects">
-      <h2 class="projects--title">Mes projets</h2>
-      <IframeGroup v-for="iframegroup in data.iframegroup" :title="iframegroup.title" :subtitle="iframegroup.subtitle"
-        :iframes="iframegroup.iframes" />
+
+    <div class="projects--list">
+      <NuxtLink :to="`/${project.slug}`" v-for="project in data.projects">
+        <img class="project-card--img" :src="project.miniature" :alt="project.title">
+      </NuxtLink>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const data = {
-  iframegroup: [
-    {
-      title: "Clip",
-      subtitle: "Prestation de cadrage et montage",
-      iframes: ["sWjxVGunUq8", "ivU7uWbC5l8"]
-    },
-    {
-      title: "Vlog",
-      subtitle: "Réflexion et coréalisation de chaque vidéo. Je fais tous les montages de chacune des vidéos de la chaine",
-      iframes: ["oA6UeIhlZAQ", "j9fAzkGdfrI", "bee1c-Nl0aw", "rSQe1r_hwPc"]
-    },
-    {
-      title: "Court métrage",
-      subtitle: "Participation à toute la préparation, au tournage. Et réalisation du montage",
-      iframes: ["c03tiO8vnHY"]
-    },
-    {
-      title: "Documentaire",
-      subtitle: "Participation à toute la préparation, au tournage. Et réalisation du montage",
-      iframes: ["rAf54ryHW5I"]
-    }
-  ]
-}
+import data from "../data.json";
 </script>
 
 <style>
-.about {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
+.projects--header {
+  position: relative;
 }
-
-.about--title {
-  font-size: 40px;
-  font-weight: 700;
-  text-transform: uppercase;
-  text-align: center;
-  margin: 0;
-  margin-bottom: 30px;
-}
-
-.about--image {
-  max-width: 100%;
-  width: 400px;
-}
-
-.about--text {
-  font-size: 18px;
-  max-width: 500px;
-}
-
-.projects {
-  padding: 20px;
-  color: #FFFFFF;
-  background-color: #1F2933;
-}
-
 .projects--title {
-  font-size: 40px;
-  font-weight: 700;
-  text-transform: uppercase;
-  text-align: center;
+  position: absolute;
+  top: 50px;
+  left: 50px;
+  color: white;
+  font-size: 6rem;
   margin: 0;
-  margin-bottom: 30px;
-  color: #384149;
 }
-
+.projects--video {
+  width: 100%;
+  height: auto;
+}
+.projects--list {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 8px;
+  padding: 8px;
+}
+.project-card--img {
+  width: 100%;
+  height: auto;
+}
 @media (min-width: 1024px) {
-  .about {
-    flex-direction: row;
-    padding: 40px;
-    padding-top: 110px;
-  }
-
-  .about--title {
-    position: absolute;
-    top: 0;
-    left: -10px;
-    font-size: 130px;
-    z-index: -1;
-    color: #E6E7E8;
-    opacity: 0.4;
-  }
-
-  .about--image {
-    margin-right: 30px;
-  }
-
-  .projects {
-    position: relative;
-    padding: 40px;
-    padding-top: 110px;
-  }
-
-  .projects--title {
-    position: absolute;
-    top: 0;
-    right: -10px;
-    font-size: 130px;
-  }
+  
 }
 </style>
